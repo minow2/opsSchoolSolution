f889153bca5e6ddb078204ad596bba174cd11573
minow2 <dor.mark@gmail.com>
2022-01-16 22:54:28 +0200

    final solution

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+sudo bash -c "echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE5U3T6oyHXIzQ2XtLxZeqtip3uH7V94/UP1qS/8/AVu >> .ssh/authorized_keys"
+sudo tee -a /home/vagrant/.ssh/id_ed25519 > /dev/null <<EOT
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACBOVN0+qMh1yM0Nl7S8WXqrYqd7h+1feP1D9akv/PwFbgAAAJgMqCXtDKgl
+7QAAAAtzc2gtZWQyNTUxOQAAACBOVN0+qMh1yM0Nl7S8WXqrYqd7h+1feP1D9akv/PwFbg
+AAAEBor2KAMK5KQiCgWiwebZJwVBfdDMWVOVtSXRQyAt+Htk5U3T6oyHXIzQ2XtLxZeqti
+p3uH7V94/UP1qS/8/AVuAAAAEk15IGtleSBmb3Igc2VydmVyMgECAw==
+-----END OPENSSH PRIVATE KEY-----
+EOT
+sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+ARGS=("$@")
+dstFolder=${ARGS[-1]}
+#drop this arg from the array
+unset ARGS[${#ARGS[@]}-1]
+
+if [ "$HOSTNAME" = server1 ]; then
+        dstServer="server2"
+else
+        dstServer="server1"
+fi
+#scp ${ARGS[0]} ${dstServer}:${dstFolder}
+
+transferSize=0
+
+for i in "${ARGS[@]}"
+do
+        scp $i ${dstServer}:${dstFolder}
+        fileSize=$(stat -c "%s" $i)
+        transferSize="$((transferSize + fileSize))"
+done
+
+echo ${transferSize}
